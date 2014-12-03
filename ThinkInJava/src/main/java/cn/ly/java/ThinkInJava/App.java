21458d0d0b76e84570313017303afda875a8a2bc
LinYongW <linyongw@gmail.com>
2014-12-03 14:46:33 +0800

    add project

---
+++ ThinkInJava/src/main/java/cn/ly/java/ThinkInJava/App.java
@@ -0,0 +1,13 @@
+package cn.ly.java.ThinkInJava;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
